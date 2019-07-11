177632c48bd18677c309ac14fffeeade01f0427f
M Towseef Hossain <towseef.22aug@gmail.com>
2019-07-11 02:18:00 -0400

    fixed the cursor on the page title

--- js/typetext.js
+++ js/typetext.js
@@ -7,21 +7,21 @@ var aText = new Array(
     var iIndex = 0; // start printing array at this posision
     var iArrLength = aText[0].length; // the length of the text array
     var iScrollAt = 20; // start scrolling up at this many lines
-    
+
     var iTextPos = 0; // initialise text position
     var sContents = ''; // initialise contents variable
     var iRow; // initialise current row
-    
+
     function typewriter()
     {
      sContents =  ' ';
      iRow = Math.max(0, iIndex-iScrollAt);
      var destination = document.getElementById("typedtext");
-    
+
      while ( iRow < iIndex ) {
       sContents += aText[iRow++] + '<br />';
      }
-     destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos) + ".";
+     destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos) + "|";
      if ( iTextPos++ == iArrLength ) {
       iTextPos = 0;
       iIndex++;
@@ -33,6 +33,6 @@ var aText = new Array(
       setTimeout("typewriter()", iSpeed);
      }
     }
-    
-    
+
+
     typewriter();
